99da9687b5c60d3631ac641cf414a43cff9a0ba3
thefreakingmind <salmansiddiqui100@gmail.com>
2023-12-30 21:00:47 +0530

    Added Word counter for compressor

---
+++ binaryTree.py
@@ -0,0 +1,11 @@
+# This is to create a binary tree for making our compressor in python 
+
+# Implement the compression algorithm for compressing the string
+class BinaryTreeCreator(object):
+    def __init__(self):
+        self.element = element 
+        self.weight = weight
+        
+ 
+    def create_binary_tree(self):
+        return None
--- reader.py
+++ reader.py
@@ -1,21 +1,32 @@
 import os
 import sys
+from collections import defaultdict 
+from binaryTree import BinaryTreeCreator
+
 
 class FileReader(object):
     def __init__(self, filename):
         self.filename = filename
+        
     
     def process_file(self):
         with open(self.filename, 'r') as f: 
             if f is None:
                 print("Error in reading File")
-                return
+                return 
             return f.read()
 
-    def process_node_name(self):
-        pass
-
-
+    # Take the String and create a frequency map 
+    def create_frequency_table(self):
+        data = {}
+        for i in self.process_file():
+            if i in data:
+                data[i] = data[i] + 1
+            else:
+                data[i] = 1 
+        return data
+    
+    
 if __name__ == '__main__':
     f = FileReader('input.txt')
-    print(f.process_file())
+    print(f.create_frequency_table())
