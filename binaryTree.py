11f0f04e40565b113f7c1dde3eddb190e03c7d94
thefreakingmind <salmansiddiqui100@gmail.com>
2023-12-31 11:56:21 +0530

    Started with a Toy Huffman Compression Algorithm

--- binaryTree.py
+++ binaryTree.py
@@ -2,10 +2,32 @@
 
 # Implement the compression algorithm for compressing the string
 class BinaryTreeCreator(object):
-    def __init__(self):
-        self.element = element 
-        self.weight = weight
+    def __init__(self, right, left):
+        self.right = right 
+        self.left = left
+
+    def children(self):
+        return (self.left, self.right)
         
- 
-    def create_binary_tree(self):
-        return None
+    def node(self):
+        return (self.left, self.right)
+        
+
+    def __str__(self):
+        return f"{self.right} + '''_'''+ {self.left}"
+    
+    def check_for_node(self):
+        pass
+
+
+def huffman_coding_tree(node, left=True, binString= ''):
+    if type(node) is str:
+        return {node: binString}
+    
+    (l, r) = node.children() 
+    d = dict() 
+    d.update(huffman_coding_tree(l, True, binString + '0'))
+    d.update(huffman_coding_tree(l, False, binString + '0'))
+    return d
+
+            
--- reader.py
+++ reader.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
+
 import os
 import sys
 from collections import defaultdict 
 from binaryTree import BinaryTreeCreator
-
+from binaryTree import huffman_coding_tree
 
 class FileReader(object):
     def __init__(self, filename):
         self.filename = filename
-        
     
     def process_file(self):
         with open(self.filename, 'r') as f: 
@@ -20,13 +22,21 @@ class FileReader(object):
     def create_frequency_table(self):
         data = {}
         for i in self.process_file():
-            if i in data:
-                data[i] = data[i] + 1
-            else:
-                data[i] = 1 
+            if str.isalpha(i):
+                if i in data :
+                    data[i] = data[i] + 1
+                else:
+                    data[i] = 1 
         return data
     
+
+    def parse_frequencies(self):
+        data = self.create_frequency_table()
+        return sorted(data.items(), key = lambda x: x[1], reverse=True)
+
     
 if __name__ == '__main__':
     f = FileReader('input.txt')
-    print(f.create_frequency_table())
+    # print(f.create_frequency_table())
+    print(f.parse_frequencies())
+
